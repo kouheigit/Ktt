36eebfa233b115790f9bff2f6de4abd95ce07d9a
kouheigit <ookawakouhei777@gmail.com>
2025-10-19 17:10:54 +0900

    feature:user_tableの追加実装

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -33,7 +33,23 @@ class CreateUsersTable extends Migration
             $table->string('company_address2')->nullable();
             $table->string('company_tel',20)->nullable();
             $table->string('company_fax',20)->nullable();
-          
+            //送付先情報
+            $table->string('send_name')->nullable();
+            $table->string('send_kana')->nullable();
+            $table->string('send_zip1',3)->nullable();
+            $table->string('send_zip2',4)->nullable();
+            $table->string('send_address1')->nullable();
+            $table->string('send_address2')->nullable();
+            $table->string('send_tel', 20)->nullable();
+
+            //システム情報
+            $table->integer('type')->default(1)->comment('1:一般,2:オーナー');
+            $table->integer('agree')->default(0)->comment('利用規約同意');
+            $table->integer('status')->default(1)->comment('1:有効,0:無効');
+            $table->integer('user_id')->nullable()->comment('親ユーザーID(オーナーの場合)');
+
+            //論理削除
+            $table->softDeletes();
         });
     }
 
