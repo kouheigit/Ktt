53d64e752ee4a3ef3fddc4c6d3b35f8b887a0779
kouheigit <ookawakouhei777@gmail.com>
2025-10-19 17:39:22 +0900

    feature:usert_tableの追加実装

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,26 +18,26 @@ class CreateUsersTable extends Migration
             $table->string('frist_name')->nullable();
             $table->string('last_kana')->nullable();
             $table->string('first_kana')->nullable();
-            $table->string('zip1',3)->nullable();
-            $table->string('zip2',4)->nullable();
-           $table->string('address1')->nullable();
-           $table->string('address2')->nullable();
-           $table->string('tel',20)->nullable();
+            $table->string('zip1', 3)->nullable();
+            $table->string('zip2', 4)->nullable();
+            $table->string('address1')->nullable();
+            $table->string('address2')->nullable();
+            $table->string('tel', 20)->nullable();
 
-           //会社情報
+            //会社情報
             $table->string('company_name')->nullable();
             $table->string('company_kana')->nullable();
             $table->string('company_zip1', 3)->nullable();
             $table->string('company_zip2', 4)->nullable();
             $table->string('company_address1')->nullable();
             $table->string('company_address2')->nullable();
-            $table->string('company_tel',20)->nullable();
-            $table->string('company_fax',20)->nullable();
+            $table->string('company_tel', 20)->nullable();
+            $table->string('company_fax', 20)->nullable();
             //送付先情報
             $table->string('send_name')->nullable();
             $table->string('send_kana')->nullable();
-            $table->string('send_zip1',3)->nullable();
-            $table->string('send_zip2',4)->nullable();
+            $table->string('send_zip1', 3)->nullable();
+            $table->string('send_zip2', 4)->nullable();
             $table->string('send_address1')->nullable();
             $table->string('send_address2')->nullable();
             $table->string('send_tel', 20)->nullable();
@@ -60,5 +60,14 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn([
+                'member_id','last_name','first_name','last_kana','first_kana',
+                 'zip1','zip2','address1','address2','tel','company_name','company_kana',
+                  'company_zip1','company_zip2','send_name','send_kana','send_zip1','send_zip2',
+                   'send_address1','send_address2','send_tel','type','agree','status','user_id',
+                    'deleted_at',
+            ]);
+        });
     }
 }
