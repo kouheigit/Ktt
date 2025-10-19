ce8259e1a9f39c07f41e5cb7ecb28c3815f23cbe
kouheigit <ookawakouhei777@gmail.com>
2025-10-19 17:55:01 +0900

    fix:user_tableの修正

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            // 会員ID
+            $table->string('member_id');
+            
+            // 個人情報
             $table->string('last_name')->nullable();
-            $table->string('frist_name')->nullable();
+            $table->string('first_name')->nullable();
             $table->string('last_kana')->nullable();
             $table->string('first_kana')->nullable();
             $table->string('zip1', 3)->nullable();
@@ -60,14 +64,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn([
-                'member_id','last_name','first_name','last_kana','first_kana',
-                 'zip1','zip2','address1','address2','tel','company_name','company_kana',
-                  'company_zip1','company_zip2','send_name','send_kana','send_zip1','send_zip2',
-                   'send_address1','send_address2','send_tel','type','agree','status','user_id',
-                    'deleted_at',
-            ]);
-        });
+        Schema::dropIfExists('users');
     }
 }
