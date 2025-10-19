c51a5a1b315966b197c29cfed352dba323ca343e
kouheigit <ookawakouhei777@gmail.com>
2025-10-19 20:13:12 +0900

    Add:Calendars_Table追加

--- src/database/migrations/2025_10_19_104331_create_calendars_table.php
+++ src/database/migrations/2025_10_19_104331_create_calendars_table.php
@@ -15,6 +15,16 @@ class CreateCalendarsTable extends Migration
     {
         Schema::create('calendars', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('hotel_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->date('date')->comment('対象日');
+            $table->date('start_date')->nullable()->comment('期間開始日');
+            $table->date('end_date')->nullable()->comment('期間終了日');
+            $table->integer('status')->default(1)->comment('1:予約可,2:予約中,3:予約済,9:休業');
+
+            //インデックス
+            $table->index(['hotel_id','date']);
+            $table->index(['user_id','start_date']);
             $table->timestamps();
         });
     }
