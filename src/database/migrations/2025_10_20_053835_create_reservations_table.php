330c2e0ddb1b026c3faf6af5f57468afc358578a
kouheigit <ookawakouhei777@gmail.com>
2025-10-20 15:11:53 +0900

    feat:reservations_tableの実装

--- src/database/migrations/2025_10_20_053835_create_reservations_table.php
+++ src/database/migrations/2025_10_20_053835_create_reservations_table.php
@@ -14,8 +14,20 @@ class CreateReservationsTable extends Migration
     public function up()
     {
         Schema::create('reservations', function (Blueprint $table) {
+            //外部キー
             $table->id();
-            $table->timestamps();
+            $table->foreignId('hotel_id')->constrained();
+            $table->foreignId('user_id')->constrained()->comment('予約者');
+            $table->foreignId('owner_id')->constrained('users')->comment('オーナー');
+            $table->foreignId('calendar_id')->nullable()->constrained();
+
+            //宿泊情報
+            $table->date('checkin_date')->comment('チェックイン日');
+            $table->date('check_out')->comment('チェックアウト日');
+            $table->time('checkin_date')->nullable()->comment('チェックイン時刻');
+            $table->time('checkin_out')->nullable()->comment('チェックアウト時刻');
+            $table->integer('days')->default(1)->comment('宿泊日数');
+          
         });
     }
 
