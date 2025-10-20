338466909f000d2beaf532ce851f38bd25a253f8
kouheigit <ookawakouhei777@gmail.com>
2025-10-20 15:34:47 +0900

    fix:reservations_tableの修正

--- src/database/migrations/2025_10_20_053835_create_reservations_table.php
+++ src/database/migrations/2025_10_20_053835_create_reservations_table.php
@@ -14,21 +14,23 @@ class CreateReservationsTable extends Migration
     public function up()
     {
         Schema::create('reservations', function (Blueprint $table) {
-            //外部キー
             $table->id();
+            
+            // 外部キー
             $table->foreignId('hotel_id')->constrained();
             $table->foreignId('user_id')->constrained()->comment('予約者');
             $table->foreignId('owner_id')->constrained('users')->comment('オーナー');
             $table->foreignId('calendar_id')->nullable()->constrained();
+            $table->foreignId('invitation_id')->nullable()->constrained();
 
-            //宿泊情報
+            // 宿泊情報
             $table->date('checkin_date')->comment('チェックイン日');
-            $table->date('check_out')->comment('チェックアウト日');
-            $table->time('checkin_date')->nullable()->comment('チェックイン時刻');
-            $table->time('checkin_out')->nullable()->comment('チェックアウト時刻');
+            $table->date('checkout_date')->comment('チェックアウト日');
+            $table->time('checkin_time')->nullable()->comment('チェックイン時刻');
+            $table->time('checkout_time')->nullable()->comment('チェックアウト時刻');
             $table->integer('days')->default(1)->comment('宿泊日数');
 
-            //ゲスト情報
+            // ゲスト情報
             $table->string('name')->nullable()->comment('代表者名');
             $table->integer('adult')->default(0)->comment('大人人数');
             $table->integer('child')->default(0)->comment('子供人数');
@@ -36,25 +38,21 @@ class CreateReservationsTable extends Migration
             $table->text('note')->nullable()->comment('備考');
 
             // 施設情報
-            $table->string('room_key',50)->nullable()->comment('入室番号');
+            $table->string('room_key', 50)->nullable()->comment('入室番号');
             $table->string('upload')->nullable()->comment('アップロードファイル');
 
-            //決済,ステータス
+            // 決済・ステータス
             $table->integer('payment')->default(0)->comment('0:現地払い, 1:クレジット');
             $table->integer('status')->default(1)->comment('ステータス');
+            
             // 論理削除
             $table->softDeletes();
             $table->timestamps();
-           //インデックス
-            $table->index(['user_id','status']);
-            $table->index(['checkin_date','status']);
-            $table->index('owner_id');
 
             // インデックス
             $table->index(['user_id', 'status']);
             $table->index(['checkin_date', 'status']);
             $table->index('owner_id');
-            
         });
     }
 
