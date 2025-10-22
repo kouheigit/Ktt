66627e4053fc590d5a545557641baf6c5d6b139b
kouheigit <ookawakouhei777@gmail.com>
2025-10-22 16:18:01 +0900

    FreedayServiceの追加実装

--- src/app/Services/FreedayService.php
+++ src/app/Services/FreedayService.php
@@ -3,13 +3,23 @@
 
 namespace App\Services;
 
+
+use App\Consts\UserConst;
 use App\Models\Freeday;
 use App\Models\User;
 use Carbon\Carbon;
 
 class FreedayService
 {
-
+    public function getFreedays($user, $date = null)
+    {
+        $user_id = $user->id;
+        if($user->type ! = UserConst::TYPE_OWNER)
+        {
+            $user_id = $user->user_id;
+        }
+       
+    }
 }
 
 
