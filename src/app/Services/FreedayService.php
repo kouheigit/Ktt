769e398fbe7ea5125484dfd4cdfd34076b6501c9
kouheigit <ookawakouhei777@gmail.com>
2025-10-22 18:39:08 +0900

    feature:Freedayの追加実装

--- src/app/Services/FreedayService.php
+++ src/app/Services/FreedayService.php
@@ -29,28 +29,42 @@ class FreedayService
                 ->get();
         }
     }
+    //フリーデイが有効かどうか
+    public function getEffectiveFreedays($user)
+    {
+        $user_id = $user->id;
+        if ($user->type != UserConst::TYPE_OWNER) {
+            $user_id = $user->user_id;
+        }
+
+        $freedays = Freeday::where('user_id', $user_id)
+            ->where('end_date', '>=', Carbon::now())
+            ->get();
+
+        return $freedays;
+    }
+
     public function getFreedaysNum($usr, $date = null)
     {
         $user_id = $user->id;
-        if($user->type != UserConst\::TYPE_OWNER)
-        {
+        if ($user->type != UserConst\::TYPE_OWNER) {
             $user_id = $user->user_id;
         }
-        if($date)
-        {
-            $freedays = Freeday::where('user_id',UserConst::TYPE_OWNER)
-            ->where('start_date','<=',$date)
-            ->where('end_date','>=',$date)
+        if ($date) {
+            $freedays = Freeday::where('user_id', UserConst::TYPE_OWNER)
+                ->where('start_date', '<=', $date)
+                ->where('end_date', '>=', $date)
                 ->get();
-        }else{
-            $freedays = Freeday::where('user_id',$user_id)
+        } else {
+            $freedays = Freeday::where('user_id', $user_id)
                 ->get();
         }
         $total_freedays = 0;
-        foreach ($freedays as $freeday)
-        {
+        foreach ($freedays as $freeday) {
             $total_freedays += $freeday->freedays
         }
+        return $total_freedays;
+    }
 }
 
 
