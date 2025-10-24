2afe0c7a2db96025cf98be210981af0adb590d1a
kouheigit <ookawakouhei777@gmail.com>
2025-10-24 22:41:39 +0900

    feat:ServiceConstの実装

--- src/app/Consts/ServiceConst.php
+++ src/app/Consts/ServiceConst.php
@@ -4,5 +4,8 @@ namespace App\Consts;
 
 class ServiceConst
 {
-
+    const TYPE_SAMEDAY = 0;
+    const TYPE_SAME_NAME ='当日予約';
+    const TYPE_PREORDER = 1;
+    const TYPE_PREORDER_NAME ='事前予約';
 }
