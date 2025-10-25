e278789c0006b5d92ddc832b21856158a7f080e4
kouheigit <ookawakouhei777@gmail.com>
2025-10-25 10:15:21 +0900

    fix:ReservationControllerの修正

--- src/app/Http/Controllers/ReservationController.php
+++ src/app/Http/Controllers/ReservationController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Models\Reservation;
@@ -15,35 +14,7 @@ use Auth;
 use DB;
 use Carbon\Carbon;
 
-class ReservationController extends Controller
-{
-    private $freeday_service;
-    public function __construct(FreedayService $freeday_service)
-    {
-        $this->freeday_service = $freeday_service;
-    }
-    public function index(Request $request)
-    {
-        $user = Auth::user();
+class ReservationController extends Controller{
 
-        //2年分のFIXDAYを取得
-        $start_date = Carbon::now()->firstOfYear();
-        $end_date = $start_date->copy()->address(2)->endOfYear();
-
-        $calendars = Calendar::where('user_id',$user->id)
-            ->whereBetween('start_date',[$start_date,$end_date])
-            ->orderBy('start_date','asc')
-            ->get();
-
-        $freedays = $this->freeday_service->getFreedays($user);
-
-        $reservations = Reservation::where('owner_id',$user->id)
-            ->whereIn('status',[
-                ReservationConst::STATUS_APPLYING,
-                ReservationConst::STATUS_UNDER_RESERVATION,
-                ReservationConst::STATUS_RESERVED
-            ])->orderBy('checkin_date','asc')->get();
-
-        return view('reservation.index',compact('calendars','freedays','reservations'));
-    }
 }
+
